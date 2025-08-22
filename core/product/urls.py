f60025017d0f0b67915ade704fbd01cad1b44f4c
Ayush  Shrestha <aayush625@gmail.com>
2025-08-22 13:57:41 +0545

    getting single category

--- core/product/urls.py
+++ core/product/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CategoryListView
+from .views import *
 
 urlpatterns =[
-    path('categories/', CategoryListView.as_view(), name='category_list')
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('categories/create/',CategoryCreateView.as_view(), name="create_category"),
+    path('categories/<int:pk>/',CategoryRetrieveView.as_view(), name="single_category")
 ]
--- core/product/views.py
+++ core/product/views.py
@@ -5,3 +5,23 @@ from .models import *
 class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class =CategorySerializer
+
+# create new category
+class CategoryCreateView(generics.CreateAPIView):
+    queryset =Category.objects.all()
+    serializer_class= CategorySerializer
+
+
+# class CategoryListCreateView(generics.ListCreateAPIView):
+#     queryset= Category.objects.all()
+#     serializer_class= CategorySerializer
+
+# get single category
+class CategoryRetrieveView(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+# delete category
+class CategoryDeleteView(generics.DestroyAPIView):
+    queryset= Category.objects.all()
+    serializer_class = CategorySerializer
