1e13f437f5197c5910252048bba40e12f96a696b
Ayush  Shrestha <aayush625@gmail.com>
2025-08-24 12:55:14 +0545

    product models created, product serializer, admin registration

--- core/product/models.py
+++ core/product/models.py
@@ -9,3 +9,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
