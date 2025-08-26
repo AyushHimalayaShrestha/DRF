6b83fe5c057a87e1389f3eb01e910e89d400423c
Ayush  Shrestha <aayush625@gmail.com>
2025-08-26 11:57:26 +0545

    RegisterUserView Created

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializer import RegisterSerializer
+from rest_framework.permissions import AllowAny
 # Create your views here.
+
+class RegisterUserView( generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes =[AllowAny]
